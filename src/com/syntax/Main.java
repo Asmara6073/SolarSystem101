d10cbb5ce3d459ffd0fbc16e0bc1aa84440d52c9
tarik-abraham <tarik.abraham97@gmail.com>
2022-05-17 20:12:05 -0400

    Feature star added but with some error that needs to be resolved

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -18,6 +18,13 @@ SolarSystem FactorX=new SolarSystem();
         Xfact.radius="10000000000";
         Xfact.heat="42989038429038492";
 
+        //adding the features for star in the solar system
+
+        FeatureStars FacX=new FeatureStars();
+        FacX.color=green;
+        FacX.radius="2423424234 km";
+        FacX.size="34424234234234 m";
+
 
 
     }
