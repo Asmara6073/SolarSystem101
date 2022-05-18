2bfe6daea415c1ec70bc8df896f6f123c07e542e
tarik-abraham <tarik.abraham97@gmail.com>
2022-05-18 19:59:59 -0400

    Planet1 features have been added successfully

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -25,6 +25,13 @@ SolarSystem FactorX=new SolarSystem();
         FacX.radius="2423424234 km";
         FacX.size="34424234234234 m";
 
+        // adding features for planet 1
+
+            Planet1Features GulK= new Planet1Features();
+
+            GulK.color="blue";
+            GulK.size="45435345353km";
+            GulK.name="preacheX";
 
 
     }
